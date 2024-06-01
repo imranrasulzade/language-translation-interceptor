9b18edd2e8d9273fb97b578575e75bcf5e2c584b
imranrasulzade <imranrasulzade5@gmail.com>
2024-06-02 02:45:16 +0400

    deleted unused imports

--- src/main/java/com/e_sys/languageinterceptor/LanguageInterceptorApplication.java
+++ src/main/java/com/e_sys/languageinterceptor/LanguageInterceptorApplication.java
@@ -1,9 +1,5 @@
 package com.e_sys.languageinterceptor;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.headers.Header;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
