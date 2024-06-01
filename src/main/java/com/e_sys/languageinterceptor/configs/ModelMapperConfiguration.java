7d5d208362fff1905ee7b596e1d4b19a9b77b868
imranrasulzade <imranrasulzade5@gmail.com>
2024-06-02 02:48:50 +0400

    three language config with model-mapper

--- src/main/java/com/e_sys/languageinterceptor/configs/ModelMapperConfiguration.java
+++ src/main/java/com/e_sys/languageinterceptor/configs/ModelMapperConfiguration.java
@@ -1,6 +1,10 @@
 package com.e_sys.languageinterceptor.configs;
 
+import com.e_sys.languageinterceptor.dto.TranslationDto;
+import com.e_sys.languageinterceptor.entities.Translation;
+import com.e_sys.languageinterceptor.responses.TranslationResponse;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +14,42 @@ public class ModelMapperConfiguration {
     public ModelMapper getModelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setPropertyCondition(new NotNullCondition<>());
+
+        modelMapper.addMappings(new PropertyMap<Translation, TranslationResponse>() {
+            @Override
+            protected void configure() {
+                using(context -> getLang(context.getSource()))
+                        .map(source, destination.getText());
+            }
+        });
+
+        modelMapper.addMappings(new PropertyMap<TranslationDto, TranslationResponse>() {
+            @Override
+            protected void configure() {
+                using(context -> getLang(context.getSource()))
+                        .map(source, destination.getText());
+            }
+        });
+
         return modelMapper;
     }
+
+    private <S> String getLang(S source) {
+        String language = LanguageInterceptor.getCurrentLanguage();
+        if (source instanceof Translation translation) {
+            return switch (language.toLowerCase()) {
+                case "en" -> translation.getEn();
+                case "ru" -> translation.getRu();
+                default -> translation.getAz();
+            };
+        } else if (source instanceof TranslationDto translationDto) {
+            return switch (language.toLowerCase()) {
+                case "en" -> translationDto.getEn();
+                case "ru" -> translationDto.getRu();
+                default -> translationDto.getAz();
+            };
+        }
+        return null;
+    }
+
 }
